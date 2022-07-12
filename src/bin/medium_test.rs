431a9d0ca02f72375f45af169bf174b3c77896bb
Gillett Hernandez <gillett.hernandez@gmail.com>
2022-07-12 06:10:41 -0700

    add medium test bin

---
+++ src/bin/medium_test.rs
@@ -0,0 +1,9 @@
+extern crate rust_pathtracer as root;
+
+use math::curves::InterpolationMode;
+use math::*;
+
+use root::curves;
+use root::parsing::curves::load_multiple_csv_rows;
+use root::world::TransportMode;
+fn main() {}
